f1e5883d698ceb37f976cee9b1be665be929f34a
qhmedhazem <45073913+qhmedhazem@users.noreply.github.com>
2021-07-12 00:38:37 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,23 +1,23 @@
 module.exports = {
 
-getFromCollection: (clusters , id) =>{
-let arr = []
-clusters.map(c => c[id]).forEach(guildsOfBots =>{
-guildsOfBots.filter(s => s).forEach(g => arr.push(g))
-})
-return arr
-},
+    getFromCollection: (clusters, id) => {
+        let arr = []
+        clusters.map(c => c[id]).forEach(guildsOfBots => {
+            guildsOfBots.filter(s => s).forEach(g => arr.push(g))
+        })
+        return arr
+    },
 
 
-getFromObject: (clusters , id) =>{
-let obj = {}
+    getFromObject: (clusters, id) => {
+        let obj = {}
 
-clusters.map(c => c[id]).forEach(object =>{
-let obj2 = Object.assign(obj , object)
-obj = obj2
-})
+        clusters.map(c => c[id]).forEach(object => {
+            let obj2 = Object.assign(obj, object)
+            obj = obj2
+        })
 
-return obj
-}
+        return obj
+    }
 
 }
